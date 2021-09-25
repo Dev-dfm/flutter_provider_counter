c88f020f1745fc522da17ea911185034ca144d1d
Dev-dfm <d.floresmedina@gmail.com>
2021-09-25 10:17:12 +0200

    ✨ Add provider for ShoppingCart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_provider_counter/providers/counter_provider.dart';
+import 'package:flutter_provider_counter/providers/shopping_cart_provider.dart';
 import 'package:flutter_provider_counter/screens/home_screen.dart';
 import 'package:flutter_provider_counter/screens/second_page.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,7 @@ void main() {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => Counter()),
+        ChangeNotifierProvider(create: (_) => ShoppingCart()),
       ],
       child: const MyApp(),
     ),
